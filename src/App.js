3d5228042ff31f385b2d941601c0f5717a3fa0d7
komit-nalashaa <komit@nalashaa.net>
2022-09-23 21:37:37 +0530

    module events and events handler

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import ExpenseItem from './components/ExpenseItem';
+import React from 'react';
+import Expenses from './components/Expenses';
 
 function App() {
   const expenses = [
@@ -24,32 +25,17 @@ function App() {
     },
   ];
 
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's Get Started!"),
+  //   React.createElement(Expenses, {items: expenses })
+  // );
+
   return (
     <div>
       <h2> Let's Get Started with our new React App!</h2>
-      <ExpenseItem 
-      title={expenses[0].title} 
-      amount={expenses[0].amount} 
-      date={expenses[0].date}
-      > </ExpenseItem>
-      <ExpenseItem 
-      title={expenses[1].title} 
-      amount={expenses[1].amount} 
-      date={expenses[1].date}
-      > </ExpenseItem>  
-      <ExpenseItem 
-      title={expenses[2].title} 
-      amount={expenses[2].amount} 
-      date={expenses[2].date}
-      > </ExpenseItem>
-      <ExpenseItem 
-      title={expenses[3].title} 
-      amount={expenses[3].amount} 
-      date={expenses[3].date}
-      > </ExpenseItem>    
-      
-
-      <p> This Paragraph is also visible!</p>
+      <Expenses item={expenses}/>  
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,22 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
+import Card from './Card';
 
 function ExpenseItem(props) {
-    
+  let title = props.title; 
+
+  const clickHandler = () => {
+    console.log("Clicked!!!");
+  }
   return (
-    <div className="expense-item">
-      
+    <Card className="expense-item">
       <div className= "expense-item__description">
-        <h2>{props.title}</h2>
+        <ExpenseDate date={props.date}/>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+      <button onClick={clickHandler}>change Title</button>
+    </Card>
   );
 }
 
