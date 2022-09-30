2a24f9637146536e65e3c54d9781f52aa07568ee
komit-nalashaa <komit@nalashaa.net>
2022-09-30 17:32:37 +0530

    some more work in events

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,22 +1,22 @@
 import React,{useState} from 'react'
 
 import ExpenseDate from './ExpenseDate';
-import './ExpenseItem.css';
 import Card from '../UI/Card';
+import './ExpenseItem.css';
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
   //function clickHandler() {}
   const [title, setTitle] = useState(props.title);
   console.log('Expense Item is evaluated by react!');
 
   const clickHandler = () => {
-    setTitle('Updated!!');
+    setTitle('Title Update Successfully!');
     console.log(title);
   }
   return (
     <Card className="expense-item">
+      <ExpenseDate date={props.date}/>
       <div className= "expense-item__description">
-        <ExpenseDate date={props.date}/>
         <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
